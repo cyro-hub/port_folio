e26500e3ee8406fd0bd3ff38317e568ebe3d083b
cyro-hub <cyrocyro2@gmail.com>
2023-04-29 18:14:07 +0300

    completed the contacts page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,109 +1,174 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import './home.scss'
-import {FaFacebookF,FaLinkedinIn,FaGithub,FaInstagram} from 'react-icons/fa'
-import {BiExit} from 'react-icons/bi'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./home.scss";
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaGithub,
+  FaInstagram,
+} from "react-icons/fa";
+import { BiExit } from "react-icons/bi";
 import { motion } from "framer-motion";
 
-
 const initial = {
-    hidden: { x: 40, opacity: 0 },
-    visible: {
-      x: 0,
-      opacity: 1
-    },
+  hidden: { y: 40, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
 };
 
-const exit = {
-    hidden: { x: 40, opacity: 0 },
-    visible: {
-      x: 0,
-      opacity: 1
-    },
+const initial1 = {
+  hidden: { x: 40, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
 };
 
 function Home() {
-
-  return (<>
-    <div className="home-container">
-        <Nav active='home'/>
+  return (
+    <>
+      <div className="home-container">
+        <Nav active="home" />
         <div className="welcome">
-            <div className="intro">
-                <h1>Hi, I'm CHRIS BARTLEY</h1>
-                <h3>Web Developer</h3>
-                <p>
-                I'm Chris Bartley, a skilled web developer proficient in HTML, CSS, React JS, C#, and SQL. With a passion for creating dynamic and user-friendly web applications, I'm committed to delivering exceptional results for your project.
-                </p>
-                <div className="actions">
-                    <a href='mailto: bartleyc935@gmail.com' className='action-hire'>Hire me</a>
-                    <Link to='/contact' className='action-talk'>Let's talk</Link>
-                </div>
+          <motion.div
+            variants={initial}
+            initial="hidden"
+            animate="visible"
+            transition={{ ease: "easeInOut", duration: 1 }}
+            className="intro">
+            <h1>Hi, I'm CHRIS BARTLEY</h1>
+            <h3>Web Developer</h3>
+            <p>
+              I'm Chris Bartley, a skilled web developer proficient in HTML,
+              CSS, React JS, C#, and SQL. With a passion for creating dynamic
+              and user-friendly web applications, I'm committed to delivering
+              exceptional results for your project.
+            </p>
+            <div className="actions">
+              <a href="mailto: bartleyc935@gmail.com" className="action-hire">
+                Hire me
+              </a>
+              <Link to="/contact" className="action-talk">
+                Let's talk
+              </Link>
             </div>
-            <div className="image-cover">
-            </div>
+          </motion.div>
+          <motion.div
+            variants={initial1}
+            initial="hidden"
+            animate="visible"
+            transition={{ ease: "easeInOut", duration: 1 }}
+            className="image-cover"></motion.div>
         </div>
-        <Footer/>
-  </div>
-  </>
-  )
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
 
-export const Footer=()=>{
-    return <div className="footer">
-    <a href="" target='blank'><FaFacebookF size={18}/></a>
-    <a href="" target='blank'><FaLinkedinIn size={18}/></a>
-    <a href="" target='blank'><FaGithub size={18}/></a>
-    <a href="" target='blank'><FaInstagram size={18}/></a>
-</div>
-}
+export const Footer = () => {
+  return (
+    <div className="footer">
+      {/* <a href="" target="blank">
+        <FaFacebookF size={18} />
+      </a> */}
+      <a
+        href="https://www.linkedin.com/in/chris-bartley-11750b274/"
+        target="blank">
+        <FaLinkedinIn size={18} />
+      </a>
+      <a href="https://github.com/cyro-hub" target="blank">
+        <FaGithub size={18} />
+      </a>
+      {/* <a href="" target="blank">
+        <FaInstagram size={18} />
+      </a> */}
+    </div>
+  );
+};
 
-export const Nav = ({active}) => {
+export const Nav = ({ active }) => {
+  const [size, setSize] = useState(window.innerWidth);
 
-    const [size,setSize]=useState(window.innerWidth);
+  const navigate = useNavigate();
 
-    const [showMenu,setShowMenu]=useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
-    const sizeChecker =()=> setSize(window.innerWidth)
+  const sizeChecker = () => setSize(window.innerWidth);
 
-    const handleShowMenu =()=>setShowMenu(!showMenu)
+  const handleShowMenu = () => setShowMenu(!showMenu);
 
-    useEffect(()=>{
-        window.addEventListener('resize',sizeChecker)
+  useEffect(() => {
+    window.addEventListener("resize", sizeChecker);
 
-        return ()=>window.removeEventListener('resize',sizeChecker)
-    })
+    return () => window.removeEventListener("resize", sizeChecker);
+  });
   return (
     <div className="nav">
-        <h3>Bartley</h3>{
-            size>645?
+      <h3 onClick={() => navigate("/")}>Bartley</h3>
+      {size > 645 ? (
         <div className="nav-links">
-            <Link to='/' className={active==='home'?'active':null}>Home</Link>
-            <Link to='/about' className={active==='about'?'active':null}>About</Link>
-            {/* <Link to='/services' className={active==='services'?'active':null}>Services</Link> */}
-            <Link to='/projects' className={active==='projects'?'active':null}>Projects</Link>
-            <Link to='/contact' className={active==='contact'?'active':null}>Contact</Link>
-        </div>:<>{
-            showMenu?<>
-                <motion.div variants={initial}
-                     initial="hidden"
-                     animate="visible"
-                     transition={{ ease: "easeInOut", duration: 1 }}
-                     className="nav-links-hamburger">
-                    <Link to='/'  className={active==='home'?'active':null}>Home</Link>
-                    <Link to='/about' className={active==='about'?'active':null}>About</Link>
-                    {/* <Link to='/services' className={active==='services'?'active':null}>Services</Link> */}
-                    <Link to='/projects' className={active==='project'?'active':null}>Projects</Link>
-                    <Link to='/contact' className={active==='contact'?'active':null}>Contact</Link>
-                </motion.div>
-                <BiExit size={22} className='exit' onClick={handleShowMenu}/>
-            </>:<><BiExit size={22} className='reverse' onClick={handleShowMenu}/></>
-            }
-            
+          <Link to="/" className={active === "home" ? "active" : null}>
+            Home
+          </Link>
+          <Link to="/about" className={active === "about" ? "active" : null}>
+            About
+          </Link>
+          {/* <Link to='/services' className={active==='services'?'active':null}>Services</Link> */}
+          <Link
+            to="/projects"
+            className={active === "projects" ? "active" : null}>
+            Projects
+          </Link>
+          <Link
+            to="/contact"
+            className={active === "contact" ? "active" : null}>
+            Contact
+          </Link>
+        </div>
+      ) : (
+        <>
+          {showMenu ? (
+            <>
+              <motion.div
+                variants={initial}
+                initial="hidden"
+                animate="visible"
+                transition={{ ease: "easeInOut", duration: 1 }}
+                className="nav-links-hamburger">
+                <Link to="/" className={active === "home" ? "active" : null}>
+                  Home
+                </Link>
+                <Link
+                  to="/about"
+                  className={active === "about" ? "active" : null}>
+                  About
+                </Link>
+                {/* <Link to='/services' className={active==='services'?'active':null}>Services</Link> */}
+                <Link
+                  to="/projects"
+                  className={active === "project" ? "active" : null}>
+                  Projects
+                </Link>
+                <Link
+                  to="/contact"
+                  className={active === "contact" ? "active" : null}>
+                  Contact
+                </Link>
+              </motion.div>
+              <BiExit size={22} className="exit" onClick={handleShowMenu} />
+            </>
+          ) : (
+            <>
+              <BiExit size={22} className="reverse" onClick={handleShowMenu} />
+            </>
+          )}
         </>
-        }
-
+      )}
     </div>
-  )
-}
+  );
+};
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,78 +1,116 @@
-import React,{useState} from 'react'
-import { Footer, Nav } from '../Home/Home'
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import background from '../../assets/Background/background.png'
-import './projects.scss'
-import '@splidejs/react-splide/css';
-import Pagination from '@mui/material/Pagination';
-import { styled} from '@mui/material/styles';
-import ProjectData from '../Data/Projects'
+import React, { useState } from "react";
+import { Footer, Nav } from "../Home/Home";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import background from "../../assets/Background/background.png";
+import "./projects.scss";
+import "@splidejs/react-splide/css";
+import Pagination from "@mui/material/Pagination";
+import { styled } from "@mui/material/styles";
+import ProjectData from "../Data/Projects";
+import { motion } from "framer-motion";
+
+const initial = {
+  hidden: { y: 40, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
+const initial1 = {
+  hidden: { x: 40, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+  },
+};
 
 function Projects() {
-const [projectNumber,setProjectNumber]=useState(1)
+  const [projectNumber, setProjectNumber] = useState(1);
   return (
     <div className="project-container">
-        <Nav active='projects'/>
-        <div className="projects">
-            <Project {...ProjectData[projectNumber-1]}/>
-        </div>
-        <CustomizedPagination count={ProjectData?.length} 
-                              onChange={(e,v)=>setProjectNumber(v)} 
-                              page={projectNumber} 
-                              color="primary" 
-                              sx={{marginInline:'auto'}} />
-        <Footer/>
+      <Nav active="projects" />
+      <div className="projects">
+        <Project {...ProjectData[projectNumber - 1]} />
+      </div>
+      <CustomizedPagination
+        count={ProjectData?.length}
+        onChange={(e, v) => setProjectNumber(v)}
+        page={projectNumber}
+        color="primary"
+        sx={{ marginInline: "auto" }}
+      />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
 
-
-const Project=({title,description,view,code,images})=>{
-    return<>
-    <div className="project">
-                <div className="details">
-                    <h3>{title}</h3>
-                    <div className="description">
-                        <p>{description}</p>
-                    </div>
-                    <div className="actions">
-                        <a href={view} className='action-hire' target='_blank'>View Live</a>
-                        <a href={code} className='action-talk' target='_blank'>Code</a>
-                    </div>
-                </div>
-                <div className="project-images">
-                    <Splide options={{
-                            rewind: true,
-                            perPage: 1,
-                            gap: '1rem',
-                            arrows: false,
-                            drag: 'free',
-                            cancelable: false
-                            }}>
-                                {
-                                    images?.map((url,i)=><SplideSlide key={i}><Image url={url}/></SplideSlide>)
-                                }
-                    </Splide>
-                </div>
-            </div>
+const Project = ({ title, description, view, code, images }) => {
+  return (
+    <>
+      <div className="project">
+        <motion.div
+          variants={initial}
+          initial="hidden"
+          animate="visible"
+          transition={{ ease: "easeInOut", duration: 1 }}
+          className="details">
+          <h3>{title}</h3>
+          <div className="description">
+            <p>{description}</p>
+          </div>
+          <div className="actions">
+            <a href={view} className="action-hire" target="_blank">
+              View Live
+            </a>
+            <a href={code} className="action-talk" target="_blank">
+              Code
+            </a>
+          </div>
+        </motion.div>
+        <motion.div
+          variants={initial1}
+          initial="hidden"
+          animate="visible"
+          transition={{ ease: "easeInOut", duration: 1 }}
+          className="project-images">
+          <Splide
+            options={{
+              rewind: true,
+              perPage: 1,
+              gap: "1rem",
+              arrows: false,
+              drag: "free",
+              cancelable: false,
+            }}>
+            {images?.map((url, i) => (
+              <SplideSlide key={i}>
+                <Image url={url} />
+              </SplideSlide>
+            ))}
+          </Splide>
+        </motion.div>
+      </div>
     </>
-}
+  );
+};
 
-const Image=({url})=>{
-    return (<>
-        <img src={url} alt="background" />
-        <div className="image-cover">
-        </div>
-    </>)
-}
+const Image = ({ url }) => {
+  return (
+    <>
+      <img src={url} alt="background" />
+      <div className="image-cover"></div>
+    </>
+  );
+};
 
 export const CustomizedPagination = styled(Pagination)`
-margin:1em auto;
-  button{
-    color:#ffffffbf;
-    border-color:#ffffffbf;
+  margin: 1em auto;
+  button {
+    color: #ffffffbf;
+    border-color: #ffffffbf;
   }
 
   button:hover {
